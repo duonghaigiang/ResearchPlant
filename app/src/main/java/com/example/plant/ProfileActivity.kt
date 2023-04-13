0dd14baabe1f844cd4771d5e9fee3c3ec77bddaa
duonghaigiang <duonghaigiang127@gmail.com>
2023-04-13 13:21:55 +0700

    update avatar storage -> update fill of users photoUrl

--- app/src/main/java/com/example/plant/ProfileActivity.kt
+++ app/src/main/java/com/example/plant/ProfileActivity.kt
@@ -1,27 +1,53 @@
 package com.example.plant
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.ktx.storage
 class ProfileActivity : AppCompatActivity() {
     private lateinit var username : TextView
-
+    private lateinit var changePassword : TextView
+    private lateinit var getImageLauncher: ActivityResultLauncher<Intent>
+    private val db = FirebaseFirestore.getInstance()
     private lateinit var emailUser : TextView
     private lateinit var btnHome : Button
+    private lateinit var btn_avatar : Button
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var btnLogout : Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activiti_profile_action)
 
+        getImageLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val imageUri = result.data?.data
+                imageUri?.let { uri ->
+                    uploadAvatar(uri)
+                }
+            }
+        }
         username = findViewById(R.id.nameuser)
         emailUser = findViewById(R.id.emailUser)
         btnHome = findViewById(R.id.btnHome)
+        btnLogout = findViewById(R.id.btnLogout)
+        btn_avatar = findViewById(R.id.btn_avatar)
+        changePassword = findViewById(R.id.changePassword)
         val currentUser = Firebase.auth.currentUser // viết tắt của kotlin
+        val user = FirebaseAuth.getInstance()
 
         currentUser?.let {
             username.text = it.displayName
@@ -32,5 +58,58 @@ class ProfileActivity : AppCompatActivity() {
         btnHome.setOnClickListener{
             startActivity(Intent(this@ProfileActivity, MainActivity::class.java))
         }
+        btnLogout.setOnClickListener{
+            user.signOut()
+            startActivity(Intent(this@ProfileActivity, LoginAction::class.java))
+        }
+        changePassword.setOnClickListener{
+            startActivity(Intent(this@ProfileActivity, ForgotPasswordAction::class.java))
+        }
+        btn_avatar.setOnClickListener {
+            val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            getImageLauncher.launch(galleryIntent)
+        }
     }
+    private fun uploadAvatar(uri: Uri) {
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        currentUser?.let { user ->
+            val storage = Firebase.storage
+            val storageRef = storage.reference
+            val avatarRef = storageRef.child("avatars/${user.uid}.jpg")
+
+            val uploadTask = avatarRef.putFile(uri)
+            uploadTask.addOnSuccessListener { taskSnapshot ->
+                avatarRef.downloadUrl.addOnSuccessListener { downloadUri ->
+                    val profileUpdates = UserProfileChangeRequest.Builder()
+                        .setPhotoUri(downloadUri)
+                        .build()
+                    user.updateProfile(profileUpdates).addOnCompleteListener { updateTask ->
+                        if (updateTask.isSuccessful) {
+                            val userHashMap = hashMapOf(
+                                "photoURL" to downloadUri.toString()
+                            )
+                            db.collection("users").document(currentUser.uid)
+                                .update(userHashMap as Map<String, Any>)
+                                .addOnSuccessListener {
+                                    Toast.makeText(this@ProfileActivity, "Avatar URL updated in Firestore.", Toast.LENGTH_SHORT).show()
+                                }
+                                .addOnFailureListener { exception ->
+                                    Toast.makeText(this@ProfileActivity, "Failed to update avatar URL in Firestore: $exception", Toast.LENGTH_SHORT).show()
+                                }
+                            Toast.makeText(this@ProfileActivity, "Avatar updated successfully.", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(this@ProfileActivity, "Failed to update avatar.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }.addOnFailureListener { exception ->
+                    Toast.makeText(this@ProfileActivity, "Failed to get download URL: $exception", Toast.LENGTH_SHORT).show()
+                }
+            }.addOnFailureListener { exception ->
+                Toast.makeText(this@ProfileActivity, "Failed to upload avatar: $exception", Toast.LENGTH_SHORT).show()
+            }
+        } ?: run {
+            Toast.makeText(this@ProfileActivity, "User not logged in", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
--- app/src/main/java/com/example/plant/SignUpAction.kt
+++ app/src/main/java/com/example/plant/SignUpAction.kt
@@ -5,6 +5,7 @@ package com.example.plant
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -12,6 +13,11 @@ import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class SignUpAction : AppCompatActivity() {
     private lateinit var name : EditText
@@ -22,7 +28,9 @@ class SignUpAction : AppCompatActivity() {
     private lateinit var signUpPassword : EditText
     private lateinit var confirmSignUpPassword : EditText
     private lateinit var forget : TextView
+    private lateinit var userID: String
 
+    private val db = FirebaseFirestore.getInstance()
     private lateinit var firebaseAuth: FirebaseAuth  //auth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,40 +52,77 @@ class SignUpAction : AppCompatActivity() {
             startActivity(Intent(this@SignUpAction, LoginAction::class.java)) //go to Login
         }
         signUpButton.setOnClickListener {
-            val name = name.text.toString();
-            var email = signUPEmail.text.toString();
-            var password = signUpPassword.text.toString();
-            var confirmPassword = confirmSignUpPassword.text.toString();
-            if (name.isNotEmpty() &&email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty())  {
-                if(email.contains("@") && password.length > 0 && confirmPassword == password)
-                {
-                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
-                        if(it.isSuccessful)
-                        {
-                            val user = firebaseAuth.currentUser   // đăng kí thành công update thêm user vừa đăng kí thêm name
-                            val profileUpdates = UserProfileChangeRequest.Builder()
-                                .setDisplayName(name)
-                                .build()
-                            user?.updateProfile(profileUpdates)
-                            var intent =Intent(this, LoginAction::class.java) // navi
-                            startActivity(intent)
-                        }
-                        else
-                        {
-                            Toast.makeText(this@SignUpAction, it.exception.toString(), Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                    Toast.makeText(this@SignUpAction, "suscess.", Toast.LENGTH_SHORT).show()
-                }
-                else
-                {
-                    Toast.makeText(this@SignUpAction, "Request not understood due to malformed syntax.", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                Toast.makeText(this@SignUpAction, "please fill in", Toast.LENGTH_SHORT).show()
-            }
+            singUp()
+
         }
 
+    }
+    private fun singUp()
+    {
+
+
+        val name = name.text.toString();
+        var email = signUPEmail.text.toString();
+        var password = signUpPassword.text.toString();
+        var confirmPassword = confirmSignUpPassword.text.toString();
+        if (name.isNotEmpty() &&email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty())  {
+            if(email.contains("@") && password.length > 0 && confirmPassword == password)
+            {
+                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val profileUpdates = UserProfileChangeRequest.Builder()
+                            .setDisplayName(name)
+                            .build()
+                        val usercurent = firebaseAuth.currentUser
+
+                        usercurent?.updateProfile(profileUpdates)?.addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                // Cập nhật hồ sơ hoàn tất
+                                userID = usercurent.uid
+
+                                val userHashMap = hashMapOf(
+                                    "email" to usercurent.email,
+                                    "displayName" to usercurent.displayName,
+                                    "numberPhone" to usercurent.phoneNumber,
+                                    "UID" to userID,
+                                    "photoURL" to usercurent.photoUrl
+                                )
+
+                                db.collection("users").document(userID)
+                                    .set(userHashMap)
+                                    .addOnSuccessListener {
+                                        // Thực hiện hành động sau khi lưu trữ dữ liệu thành công
+                                        navi()
+                                    }
+                                    .addOnFailureListener { exception ->
+                                        // Xử lý lỗi khi lưu trữ dữ liệu thất bại
+                                        Toast.makeText(this@SignUpAction, exception.toString(), Toast.LENGTH_SHORT).show()
+                                    }
+                            } else {
+                                // Xử lý lỗi khi cập nhật hồ sơ không thành công
+                                Toast.makeText(this@SignUpAction, "Failed to update profile.", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+
+                    } else {
+                        Toast.makeText(this@SignUpAction, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+                Toast.makeText(this@SignUpAction, "suscess.", Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                Toast.makeText(this@SignUpAction, "Request not understood due to malformed syntax.", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(this@SignUpAction, "please fill in", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun navi() {
+        val intent = Intent(this, LoginAction::class.java) // navi
+        startActivity(intent)
     }
 
 
