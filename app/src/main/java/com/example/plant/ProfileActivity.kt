8878ca898537424516798720a2bea8b18c38d24f
duonghaigiang <duonghaigiang127@gmail.com>
2023-04-13 20:21:58 +0700

    lib Gilde render avatar

--- app/src/main/java/com/example/plant/ProfileActivity.kt
+++ app/src/main/java/com/example/plant/ProfileActivity.kt
@@ -1,5 +1,4 @@
 package com.example.plant
-
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -10,12 +9,16 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import android.provider.MediaStore
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.ktx.storage
+import com.bumptech.glide.Glide
+import com.google.firebase.firestore.DocumentSnapshot
+
 class ProfileActivity : AppCompatActivity() {
     private lateinit var username : TextView
     private lateinit var changePassword : TextView
@@ -26,6 +29,8 @@ class ProfileActivity : AppCompatActivity() {
     private lateinit var btn_avatar : Button
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var btnLogout : Button
+
+    private lateinit var avatar : ImageView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,6 +51,8 @@ class ProfileActivity : AppCompatActivity() {
         btnLogout = findViewById(R.id.btnLogout)
         btn_avatar = findViewById(R.id.btn_avatar)
         changePassword = findViewById(R.id.changePassword)
+        avatar = findViewById(R.id.avatar)
+
         val currentUser = Firebase.auth.currentUser // viết tắt của kotlin
         val user = FirebaseAuth.getInstance()
 
@@ -69,6 +76,7 @@ class ProfileActivity : AppCompatActivity() {
             val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             getImageLauncher.launch(galleryIntent)
         }
+        loadAvatarUrl(currentUser?.uid)
     }
     private fun uploadAvatar(uri: Uri) {
         val currentUser = FirebaseAuth.getInstance().currentUser
@@ -88,11 +96,21 @@ class ProfileActivity : AppCompatActivity() {
                             val userHashMap = hashMapOf(
                                 "photoURL" to downloadUri.toString()
                             )
+
+
                             db.collection("users").document(currentUser.uid)
                                 .update(userHashMap as Map<String, Any>)
                                 .addOnSuccessListener {
+
+                                    Glide.with(this@ProfileActivity)
+                                        .load(downloadUri) // hiện avatar vừa nhập
+                                        .into(avatar)
+
+
                                     Toast.makeText(this@ProfileActivity, "Avatar URL updated in Firestore.", Toast.LENGTH_SHORT).show()
+
                                 }
+
                                 .addOnFailureListener { exception ->
                                     Toast.makeText(this@ProfileActivity, "Failed to update avatar URL in Firestore: $exception", Toast.LENGTH_SHORT).show()
                                 }
@@ -109,6 +127,25 @@ class ProfileActivity : AppCompatActivity() {
             }
         } ?: run {
             Toast.makeText(this@ProfileActivity, "User not logged in", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun loadAvatarUrl(uid: String?) {
+        if (uid != null) {
+            db.collection("users").document(uid)
+                .get()
+                .addOnSuccessListener { documentSnapshot: DocumentSnapshot ->
+                    val photoURL = documentSnapshot.getString("photoURL")
+                    if (photoURL != null) {
+                        Glide.with(this)
+                            .load(photoURL)
+                            .placeholder(R.drawable.ic_baseline_account_circle_24) // Hiển thị ảnh đại diện mặc định khi đang tải
+                            .error(R.drawable.ic_baseline_account_circle_24) // Hiển thị ảnh đại diện mặc định nếu tải ảnh gặp lỗi
+                            .into(avatar)
+                    }
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(this, "Error loading avatar: $e", Toast.LENGTH_SHORT).show()
+                }
         }
     }
 
