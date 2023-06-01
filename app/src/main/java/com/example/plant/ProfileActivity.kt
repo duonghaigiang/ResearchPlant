ad9e7047c88151427e2d50d3e74f3f3171fd04ae
duonghaigiang <duonghaigiang127@gmail.com>
2023-06-02 01:59:05 +0700

    finally

--- app/src/main/java/com/example/plant/ProfileActivity.kt
+++ app/src/main/java/com/example/plant/ProfileActivity.kt
@@ -2,24 +2,31 @@ package com.example.plant
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import android.provider.MediaStore
-import android.widget.ImageView
-import android.widget.Toast
+import android.util.Log
+import android.view.View
+import android.widget.*
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.ktx.storage
 import com.bumptech.glide.Glide
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.Query
+import java.text.SimpleDateFormat
+import java.util.*
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() , ArticleAdapter.OnArticleClickListener{
     private lateinit var username : TextView
     private lateinit var changePassword : TextView
     private lateinit var getImageLauncher: ActivityResultLauncher<Intent>
@@ -29,9 +36,10 @@ class ProfileActivity : AppCompatActivity() {
     private lateinit var btn_avatar : Button
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var btnLogout : Button
-
+    private lateinit var articleAdapter: ArticleAdapter
+    private val articleList: MutableList<Article> = mutableListOf()
     private lateinit var avatar : ImageView
-
+    private lateinit var articlesRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -70,8 +78,17 @@ class ProfileActivity : AppCompatActivity() {
             val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             getImageLauncher.launch(galleryIntent)
         }
+
+        // Initialize the RecyclerView, LinearLayoutManager, and ArticleAdapter
+        articlesRecyclerView = findViewById(R.id.articlesRecyclerView)
+        val layoutManager = LinearLayoutManager(this)
+        articleAdapter = ArticleAdapter(articleList, this, this)
+        // Set up the RecyclerView with the adapter and layout manager
+        articlesRecyclerView.adapter = articleAdapter
+        articlesRecyclerView.layoutManager = layoutManager
         loadAvatarUrl(currentUser?.uid)
         loadNumberOfArticles(currentUser?.uid)
+        fetchArticles()
     }
     private fun loadNumberOfArticles(uid: String?) {
         val articlesCountView : TextView = findViewById(R.id.articlesCount) // replace this with the id of your TextView
@@ -97,6 +114,53 @@ class ProfileActivity : AppCompatActivity() {
         } else {
             articlesCountView.text = "0"
         }
+    }
+    private fun fetchArticles() {
+    //    val currentUser = FirebaseAuth.getInstance().currentUser
+        val progressBar: ProgressBar = findViewById(R.id.progressBar)
+       // val currentUserId = currentUser?.uid
+// Trước khi tải dữ liệu
+        progressBar.visibility = View.VISIBLE
+
+        val db = FirebaseFirestore.getInstance()
+        val auth = Firebase.auth
+        val currentUserId = Firebase.auth.currentUser?.uid
+        db.collection("articles")
+            .whereArrayContains("likes", currentUserId ?: "")  // Retrieve only articles that the current user has liked
+
+            .get()
+            .addOnSuccessListener { documents ->
+                val userFetchTasks = mutableListOf<Task<DocumentSnapshot>>()
+
+                for (document in documents) {
+                    val uid = document.getString("uid") ?: ""
+                    val userFetchTask = db.collection("users").document(uid).get()
+                    userFetchTasks.add(userFetchTask)
+
+                    userFetchTask.addOnSuccessListener { userDocument ->
+                        val author = userDocument.getString("displayName") ?: ""
+                        val title = document.getString("title") ?: ""
+                        val description = document.getString("description") ?: ""
+                        val imageUrl = document.getString("imgURL") ?: ""
+                        val createdAt = document.getTimestamp("createdAt") ?: Timestamp.now()
+                        val avatar = userDocument.getString("photoURL") ?: ""
+                        val email = userDocument.getString("email") ?: ""
+                        val id = document.id
+                        val article = Article(id, avatar, author, email, title, description, imageUrl, createdAt)
+                        articleList.add(article)
+                    }
+                }
+                Tasks.whenAllSuccess<DocumentSnapshot>(userFetchTasks).addOnSuccessListener {
+                    articleAdapter.notifyDataSetChanged()
+                    progressBar.visibility = View.GONE
+                }
+                Log.d("fetchArticles", "Fetched ${documents.size()} documents")
+            }
+            .addOnFailureListener { exception ->
+            Log.e("fetchArticles", "Failed to fetch articles: $exception", exception)
+            //...
+        }
+
     }
     private fun uploadAvatar(uri: Uri) {
         val currentUser = FirebaseAuth.getInstance().currentUser
@@ -173,6 +237,11 @@ class ProfileActivity : AppCompatActivity() {
         btn_avatar = findViewById(R.id.btn_avatar)
         changePassword = findViewById(R.id.changePassword)
         avatar = findViewById(R.id.avatar)
+    }
+    override fun onArticleClick(article: Article) {
+        val intent = Intent(this, DetailArticle_Action::class.java)
+        intent.putExtra("article", article)
+        startActivity(intent)
     }
 
 }
